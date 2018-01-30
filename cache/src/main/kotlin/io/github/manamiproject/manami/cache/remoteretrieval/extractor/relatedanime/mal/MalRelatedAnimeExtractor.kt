918b415be70eadf55cdb261496de37bec7cfebf9
manami.project <manami.project@posteo.net>
2018-01-30 16:42:20 +0100

    Kotlin: Relared anime extractor for MAL

--- cache/src/main/kotlin/io/github/manamiproject/manami/cache/remoteretrieval/extractor/relatedanime/mal/MalRelatedAnimeExtractor.kt
+++ cache/src/main/kotlin/io/github/manamiproject/manami/cache/remoteretrieval/extractor/relatedanime/mal/MalRelatedAnimeExtractor.kt
@@ -3,10 +3,41 @@ package io.github.manamiproject.manami.cache.remoteretrieval.extractor.relatedan
 import io.github.manamiproject.manami.cache.remoteretrieval.extractor.relatedanime.RelatedAnimeExtractor
 import io.github.manamiproject.manami.dto.entities.DOMAINS
 import io.github.manamiproject.manami.dto.entities.InfoLink
+import org.cyberneko.html.parsers.DOMParser
+import org.w3c.dom.NodeList
+import org.xml.sax.InputSource
+import java.io.StringReader
+import javax.xml.xpath.XPathConstants
+import javax.xml.xpath.XPathFactory
 
 internal class MalRelatedAnimeExtractor : RelatedAnimeExtractor {
+
+    private val xPath = XPathFactory.newInstance().newXPath()
+    private val parser = DOMParser().apply {
+        setFeature("http://xml.org/sax/features/namespaces", false)
+    }
+
     override fun extractRelatedAnime(html: String): MutableSet<InfoLink> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val relatedAnime: MutableSet<InfoLink> = mutableSetOf()
+
+        parser.parse(InputSource(StringReader(html)))
+
+        val doc = parser.document
+        val relatedAnimeLinks: NodeList = xPath.evaluate("//TABLE[@class='anime_detail_related_anime']//TR[./TD/text()!='Adaptation:']//A/@href", doc.documentElement, XPathConstants.NODESET) as NodeList
+
+        if(relatedAnimeLinks.length == 0) {
+            return relatedAnime
+        }
+
+        (0 until relatedAnimeLinks.length)
+                .map { relatedAnimeLinks.item(it) }
+                .forEach {
+                    Regex("\\/\\d+\\/").find(it.textContent)?.let { matchResult ->
+                        relatedAnime.add(InfoLink("https://${DOMAINS.MAL.value}/${matchResult.value}"))
+                    }
+                }
+
+        return relatedAnime
     }
 
     override fun isResponsible(infoLink: InfoLink) = infoLink.toString().contains(DOMAINS.MAL.value)
