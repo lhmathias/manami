4dea144357fc092e1660591cbe8afaa6e893ad45
manami.project <manami.project@posteo.net>
2018-05-01 20:35:23 +0200

    Kotlin: switched to collections shuffle

--- core/src/main/kotlin/io/github/manamiproject/manami/core/ListRandomizer.kt
+++ core/src/main/kotlin/io/github/manamiproject/manami/core/ListRandomizer.kt
@@ -1,13 +1,16 @@
 package io.github.manamiproject.manami.core
 
 import java.security.SecureRandom
+import java.util.Collections.shuffle
 
 object ListRandomizer {
 
-    fun randomizeOrder(list: MutableList<out Any>) {
-        list.shuffle(SecureRandom())
-        list.shuffle(SecureRandom())
-        list.shuffle(SecureRandom())
-        list.shuffle(SecureRandom())
+    fun <T> randomizeOrder(list: List<T>): List<T> {
+        val mutableList = list.toMutableList()
+        shuffle(mutableList, SecureRandom())
+        shuffle(mutableList, SecureRandom())
+        shuffle(mutableList, SecureRandom())
+        shuffle(mutableList, SecureRandom())
+        return mutableList.toList()
     }
 }
