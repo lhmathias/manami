c29c1f277324db09275a03c3c7916a70a592621c
manami.project <manami.project@posteo.net>
2018-05-01 19:55:47 +0200

    Kotlin: created a related anime finder for direct relations only and for transitive relations

--- core/src/main/kotlin/io/github/manamiproject/manami/core/tasks/AllRelatedAnimeFinderTask.kt
+++ core/src/main/kotlin/io/github/manamiproject/manami/core/tasks/AllRelatedAnimeFinderTask.kt
@@ -1,18 +1,17 @@
 package io.github.manamiproject.manami.core.tasks
 
 import io.github.manamiproject.manami.cache.Cache
-import io.github.manamiproject.manami.common.EventBus
 import io.github.manamiproject.manami.common.LoggerDelegate
-import io.github.manamiproject.manami.core.tasks.events.ProgressState
 import io.github.manamiproject.manami.dto.entities.InfoLink
 import io.github.manamiproject.manami.persistence.Persistence
 import org.slf4j.Logger
 
+
 /**
- * Finds all directly related anime for infolinks.
+ * Finds all related anime for infolinks, not only the directly related.
  * Always start {@link BackgroundTask}s using the {@link ServiceRepository}!
  */
-internal class RelatedAnimeFinderTask(
+internal class AllRelatedAnimeFinderTask(
         private val cache: Cache,
         private val persistence: Persistence,
         private val list: List<InfoLink>
@@ -21,19 +20,17 @@ internal class RelatedAnimeFinderTask(
     private val log: Logger by LoggerDelegate()
 
     override fun execute() {
-        var counter = 0
+        val directlyRelatedAnime: Set<InfoLink> = mutableSetOf()
 
         val relatedAnimeResult: Set<InfoLink> =  list.map {
-            EventBus.publish(ProgressState(++counter, list.size))
             cache.fetchRelatedAnime(it)
-        }
-        .flatten()
-        .filter { it.isValid() }
+        }.toSet()
+
+        directlyRelatedAnime.filter { it.isValid() }
         .filter { !persistence.animeEntryExists(it) }
         .filter { !persistence.watchListEntryExists(it) }
         .filter { !persistence.filterListEntryExists(it) }
-        .toHashSet()
+        .filter { !relatedAnimeResult.contains(it) }
 
-        //TODO: fire event with result
     }
 }
